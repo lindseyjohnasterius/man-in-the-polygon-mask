4e444b0200ec24bc3a92e17d059bf453e6af79f4
mr lindsey carl mysse <lindsey.mysse@gmail.com>
2021-12-23 16:27:39 -0800

    added html partial

---
+++ assets/vendor/html-partial.js
@@ -0,0 +1,22 @@
+class HTMLPartial extends HTMLElement {
+
+ connectedCallback(){
+   this.shadowdom = this.getAttribute('shadowdom')
+   this.src = this.getAttribute('src')
+   if(this.src === null){
+      this.innerHTML = '<error>HTML PARTIAL REQUIRES SOURCE</error>'
+   }
+   fetch(this.src)
+    .then(res => res.text())
+    .then(res => {
+      if(this.shadowdom){
+        this.shadow = this.attachShadow({mode: 'open'})
+        this.shadow.innerHTML = res
+      } else {
+        this.innerHTML = res
+      }
+    })
+  }
+}
+
+customElements.define('html-partial', HTMLPartial)
